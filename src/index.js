05a5af837c8f60f079f9a506e00320025c0d3ebe
Swastika <swastikadhakal37@gmail.com>
2026-01-31 22:12:12 +0545

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+import{DB_NAME} from "./constants.js"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import express from "express";
+// const app=express()
+// ;(async()=>{
+//     try{
+//         await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+//         app.on("error",
+//             (error)=>{console.error("Error in server connection:",error)}
+//         )
+// app.listen(process.env.PORT,()=>{
+//     console.log(`App is Listing on port ${process.env.PORT}`);
+// })
+//     }
+//     catch(error){
+//         console.error("Database connection error:", error);
+//         throw
+
+//     }
+// })()

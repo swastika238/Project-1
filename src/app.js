e5efee0b5603100216611bb302b444545d5c629c
Swastika <swastikadhakal37@gmail.com>
2026-02-02 23:43:02 +0545

    Initial Commit

--- src/app.js
+++ src/app.js
@@ -1,6 +1,14 @@
 import express from 'express';
+import cors from "cors";
+import cookieParser from 'cookie-parser';
+
 
 const app=express()
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+}));
 
 
 export default app;
+// CORS=Cross Origin RESOURCE SHARING
